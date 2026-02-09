aa7be891766d58dcb1fad2db06e7e9db87bf2738
aallen27 <aallen27@yc.edu>
2026-02-09 01:24:32 -0700

    New Lines

---
+++ handle_post2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Forum Posting</title>
+    </head>
+    <body>
+        <!-- Script 5.2 - handle_post.php -->
+        <?php
+            $first_name = $_POST['first_name'];
+            $last_name = $_POST['last_name'];
+            $posting = nl2br($_POST['posting'], false);
+
+            $name = $first_name . ' ' . $last_name;
+            print "<div> Thank you, $name, for your posting: <p>$posting</p></div>";
+        ?>
+    </body>
+</html>
