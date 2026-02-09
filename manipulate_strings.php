2c93a9fd1e6236fae868db8aaa62022f7d6d0c08
aallen27 <aallen27@yc.edu>
2026-02-09 04:38:44 -0700

    substrings

--- manipulate_strings.php
+++ manipulate_strings.php
@@ -7,9 +7,21 @@
     <body>
         <?php
             $document = "This is an example text used to test various string operations to see if they are practical";
+            
+            // Gets a substring from the document that only includes the word 'test' and all of the words after it.
             $second_half = strstr($document, "test"); 
-            $first_half = str_replace($second_half, '', $document); //Super sketchy. Avoid at all costs, due to the fact that it reads through the entire string, can fail if the seconds half is small and repeated, and that strstr() has an argument that can do the same thing easily. This is only to show it is possible
+
+            //Super sketchy way to get the first half of the string by erasing the second half.
+            //Avoid at all costs, due to the fact that it reads through the entire string, 
+            //  can fail if the seconds half is small and repeated, 
+            //  and that strstr() has an argument that can do the same thing easily. 
+            //This is only to show it is possible
+            $first_half = str_replace($second_half, '', $document); 
+
+            // Compares the two halfs of the string to see which is longer
             $longer = strcmp($first_half, $second_half);
+
+            //displays the results
             print "The first half of '$first_half' is longer than the second half '$second_half' if '$longer' is positive."
         ?>
     </body>
