2e5f4e57350e0dadc14cf7ce5b874dd390ddb396
aallen27 <aallen27@yc.edu>
2026-02-22 23:04:00 -0700

    Using External Files

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php // Script 8.4 - index.php
+
+//Include the header:
+include('templates/header.html');
+?>
+
+<h2>Index.php title text</h2>
+<p>Content data</p>
+
+<?php //Return to PHP to include the footer
+include('templates/footer.html');
+?>
