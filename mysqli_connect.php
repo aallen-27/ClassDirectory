920ba72f48a0dfc7e5a1154b38bafbe8a69be09c
aallen27 <aallen27@yc.edu>
2026-03-08 22:29:02 -0700

    Connecting to MySQL PHP code

---
+++ mysqli_connect.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <title>Connect to MySQL</title>
+    </head>
+    <body>
+        <?php // Script 12.1 mysqli_connect.php
+            // Store the password in a file so that it is not available in GitHub
+            $passfile = fopen('/opt/lampp/data/sql_pass.txt', 'r');
+            $password = trim(fread($passfile, filesize('/opt/lampp/data/sql_pass.txt')));
+
+            // connect to the database
+            if ($dbc = @mysqli_connect('localhost', 'root', "$password", 'myblog')) {
+                print '<p>Successfully connected to the database!</p>';
+                mysqli_close($dbc);
+            } else {
+                print '<p style="color: red;">Could not connect to the database.<br>' . 
+                    mysqli_connect_error() .'</p>';
+            }
+        ?>
+    </body>
+</html>
